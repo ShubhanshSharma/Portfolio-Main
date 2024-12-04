c4b4bc451c811367d16eb0e86b7782e49973c568
Shubhansh Sharma <shubhanshsharma030604@gmail.com>
2024-12-05 01:20:03 +0530

    4 december

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -70,6 +70,14 @@ export default function Navbar(){
         <div style={{mixBlendMode:'difference'}} className="FIRA fixed h-[150px] rounded-full items-end mt-[-100px] z-[3] pb-2 w-11/12 flex flex-row group justify-end  gap-16 lg:px-32"
             onMouseEnter={() => setNavbarHover(true)} onMouseLeave={() => setNavbarHover(false)} >
 
+            <a href='https://www.youtube.com/playlist?list=PLG3Op_4yeGuAVG9M8RGPxHNwaJT3U1V0E' className='NAV_BUTTONS CURSOR_SCALE_DOWN mix-blend-difference text-neutral-100 relative w-fit px-3 py-1 overflow-clip ' 
+                onMouseEnter={() => handleButtonHover(0)} 
+                onMouseLeave={() => handleButtonHover(0)}>
+                <span>Data Science</span>
+                <div className='NAV_BUTTON_MOVE_RIGHT0 h-0 w-full border-b-[1px] border-neutral-50 absolute left-0  bottom-0 rounded-full'></div>
+                {/* <div className='NAV_BUTTON_MOVE_DOWN0 w-0 h-[70%] border-r-[1px] border-neutral-50 absolute left-0 bottom-0 rounded-full '></div> */}
+            </a>
+
             <a href='#about' className='NAV_BUTTONS CURSOR_SCALE_DOWN mix-blend-difference text-neutral-100 relative w-fit px-3 py-1 overflow-clip ' 
             onMouseEnter={() => handleButtonHover(0)} 
             onMouseLeave={() => handleButtonHover(0)}>
